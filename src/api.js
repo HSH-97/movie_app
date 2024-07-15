e6d27194f1f2751a595484eaf0ecf502ed84ba80
HSH-97 <shh6297@gmail.com>
2024-07-15 16:41:19 +0900

    movie_list_setup

---
+++ src/api.js
@@ -0,0 +1,27 @@
+const fetch = require("node-fetch");
+
+const baseUrl = "https://api.themoviedb.org/3/";
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNTY3Mzc5YzllYmZjMWZkMWE0ZGVmOWVkMzNiMTIyOSIsIm5iZiI6MTcyMDY4NzI3MS41OTgxMjUsInN1YiI6IjY2OGY5NjU0ZGViZTk2MzVkMmU2YTBlNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rWnusAG91QLJcBS_WHa9TEOm_otRJVEvJGt8-4kWKSs",
+  },
+};
+
+const url = (urlName) => {
+  return baseUrl + `${urlName}?languege=ko-kr`;
+};
+
+export const nowPlaying = () =>
+  fetch(url("movie/now_playing"), options).then((res) => res.json());
+
+export const popular = () =>
+  fetch(url("movie/popular"), options).then((res) => res.json());
+
+export const topRated = () =>
+  fetch(url("movie/top_rated"), options).then((res) => res.json());
+
+export const upcoming = () =>
+  fetch(url("movie/upcoming"), options).then((res) => res.json());
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,3 +1,31 @@
+import { useEffect, useState } from "react";
+import { nowPlaying, popular, topRated, upcoming } from "../../api";
+
 export const Home = () => {
-  return <div>얍!!</div>;
+  const [nowData, setNowData] = useState();
+  const [popData, setPopData] = useState();
+  const [topData, setTopData] = useState();
+  const [upData, setUpData] = useState();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { results: nowResult } = await nowPlaying();
+        const { results: popResult } = await popular();
+        const { results: topResult } = await topRated();
+        const { results: upResult } = await upcoming();
+
+        setNowData(nowResult);
+        setPopData(popResult);
+        setTopData(topResult);
+        setUpData(upResult);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  // console.log(nowData);
+
+  return <div>Home</div>;
 };
